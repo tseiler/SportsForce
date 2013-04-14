cfa38ec92bb698a950022d1091a84dfd5d9479bd
Travis Seiler <tseiler@xcellutions.com>
2013-04-14 13:22:49 -0400

    First round of creating JavaDoc. Mostly just for entity classes.

--- src/jsportsreg/entity/Division.java
+++ src/jsportsreg/entity/Division.java
@@ -12,78 +12,158 @@ import java.util.List;
 
 
 /**
- * The persistent class for the Divisions database table.
+ * The class for the Divisions database table.
  * 
  */
 
 public class Division{
 
 
+	/** The primary key for the Division class.
+	 * 
+	 */
 	private int divisionID;
+	
+	/** The description for the Division object.
+	 * 
+	 */
 	private String divisionDescription;
+	
+	/** The name of the division.
+	 * 
+	 */
 	private String divisionName;
 
-	//bi-directional many-to-one association to Season
-
+	/** The Season the Division competes during.
+	 * 
+	 */
 	private Season season;
 
-	//bi-directional many-to-one association to Sport
+	/** The sport the Division is associated with.
+	 * 
+	 */
 	private Sport sport;
 
-	//bi-directional many-to-one association to Player_Registration
+	/** The list of Players participating in the associated division.
+	 * 
+	 */
 	private List<Player_Registration> playerRegistrations;
 
+	/** Constructor to create a basic empty Division object.
+	 * 
+	 */
 	public Division() {
 	}
 
+	/** Constructor to create a fully populated Division object.
+	 * @param divisionID
+	 * @param divisionDescription
+	 * @param divisionName
+	 * @param season
+	 * @param sport
+	 * @param playerRegistrations
+	 */
+	public Division(int divisionID, String divisionDescription,
+			String divisionName, Season season, Sport sport,
+			List<Player_Registration> playerRegistrations) {
+		this.divisionID = divisionID;
+		this.divisionDescription = divisionDescription;
+		this.divisionName = divisionName;
+		this.season = season;
+		this.sport = sport;
+		this.playerRegistrations = playerRegistrations;
+	}
+
+
+	/** Returns the division identifier, which is the primary key of the division table in the database.
+	 * This field is not needed during general use of the class.
+	 * @return
+	 */
 	public int getDivisionID() {
 		return this.divisionID;
 	}
 
+	/** Sets the division identifier.
+	 * @param divisionID
+	 */
 	public void setDivisionID(int divisionID) {
 		this.divisionID = divisionID;
 	}
 
+	/** Returns the description of the Division.
+	 * @return
+	 */
 	public String getDivisionDescription() {
 		return this.divisionDescription;
 	}
 
+	/** Sets the division description.
+	 * @param divisionDescription
+	 */
 	public void setDivisionDescription(String divisionDescription) {
 		this.divisionDescription = divisionDescription;
 	}
 
+	/** Returns the name of the division.
+	 * @return
+	 */
 	public String getDivisionName() {
 		return this.divisionName;
 	}
 
+	/** Sets the name of the division.
+	 * @param divisionName
+	 */
 	public void setDivisionName(String divisionName) {
 		this.divisionName = divisionName;
 	}
 
+	/** Returns the season associated with the division.
+	 * @return
+	 */
 	public Season getSeason() {
 		return this.season;
 	}
 
+	/** Sets the season the division competes during.
+	 * @param season
+	 */
 	public void setSeason(Season season) {
 		this.season = season;
 	}
 
+	/** Returns the sport associated with the division.
+	 * @return
+	 */
 	public Sport getSport() {
 		return this.sport;
 	}
 
+	/** Sets the sport associated with the division.
+	 * @param sport
+	 */
 	public void setSport(Sport sport) {
 		this.sport = sport;
 	}
 
+	/** Returns the list of players registered for this division.
+	 * @return
+	 */
 	public List<Player_Registration> getPlayerRegistrations() {
 		return this.playerRegistrations;
 	}
 
+	/** Sets the list of players registered for this division.
+	 * @param playerRegistrations
+	 */
 	public void setPlayerRegistrations(List<Player_Registration> playerRegistrations) {
 		this.playerRegistrations = playerRegistrations;
 	}
 
+	/** Adds a player to the list of registered players.
+	 * @param playerRegistration
+	 * @return
+	 */
 	public Player_Registration addPlayerRegistration(Player_Registration playerRegistration) {
 		getPlayerRegistrations().add(playerRegistration);
 		playerRegistration.setDivision(this);
@@ -91,6 +171,10 @@ public class Division{
 		return playerRegistration;
 	}
 
+	/** Removes a player from the list of registered players.
+	 * @param playerRegistration
+	 * @return
+	 */
 	public Player_Registration removePlayerRegistration(Player_Registration playerRegistration) {
 		getPlayerRegistrations().remove(playerRegistration);
 		playerRegistration.setDivision(null);
--- src/jsportsreg/entity/Season.java
+++ src/jsportsreg/entity/Season.java
@@ -24,61 +24,122 @@ public class Season{
 	 */
 	private int seasonID;
 
-	/** the 
+	/** The date the season ends.
 	 * 
 	 */
 	private Date seasonEndDate;
 
+	/** The general name of the season.
+	 * 
+	 */
 	private String seasonName;
 	
+	/** The date the season starts.
+	 * 
+	 */
 	private Date seasonStartDate;
 
-	//bi-directional many-to-one association to Division
+	/** A list of all divisions that participate during the season. 
+	 * 
+	 */
 	private List<Division> divisions;
 
+	/** Basic constructor to create empty object.
+	 * 
+	 */
 	public Season() {
 	}
+	
+	/** Constructor that creates a fully populated Season object.
+	 * @param seasonID
+	 * @param seasonEndDate
+	 * @param seasonName
+	 * @param seasonStartDate
+	 * @param divisions
+	 */
+	public Season(int seasonID, Date seasonEndDate, String seasonName,
+			Date seasonStartDate, List<Division> divisions) {
+		this.seasonID = seasonID;
+		this.seasonEndDate = seasonEndDate;
+		this.seasonName = seasonName;
+		this.seasonStartDate = seasonStartDate;
+		this.divisions = divisions;
+	}
 
+	/** Returns the season ID, this is the primary key of the database table.
+	 * @return
+	 */
 	public int getSeasonID() {
 		return this.seasonID;
 	}
 
+	/** Sets the season ID.
+	 * @param seasonID
+	 */
 	public void setSeasonID(int seasonID) {
 		this.seasonID = seasonID;
 	}
 
+	/** Returns the end date of the season.
+	 * @return
+	 */
 	public Date getSeasonEndDate() {
 		return this.seasonEndDate;
 	}
 
+	/** Sets the end date of the season.
+	 * @param seasonEndDate
+	 */
 	public void setSeasonEndDate(Date seasonEndDate) {
 		this.seasonEndDate = seasonEndDate;
 	}
 
+	/** Returns the general name of the season.
+	 * @return
+	 */
 	public String getSeasonName() {
 		return this.seasonName;
 	}
 
+	/** Sets the general name of the season.
+	 * @param seasonName
+	 */
 	public void setSeasonName(String seasonName) {
 		this.seasonName = seasonName;
 	}
 
+	/** Returns the seasons start date.
+	 * @return
+	 */
 	public Date getSeasonStartDate() {
 		return this.seasonStartDate;
 	}
 
+	/** Sets the seasons start date.
+	 * @param seasonStartDate
+	 */
 	public void setSeasonStartDate(Date seasonStartDate) {
 		this.seasonStartDate = seasonStartDate;
 	}
 
+	/** Returns the list of divisions participating during the season.
+	 * @return
+	 */
 	public List<Division> getDivisions() {
 		return this.divisions;
 	}
 
+	/** Sets the list of divisions that participate during the season.
+	 * @param divisions
+	 */
 	public void setDivisions(List<Division> divisions) {
 		this.divisions = divisions;
 	}
 
+	/** Adds a division to the list participating during the season.
+	 * @param division
+	 * @return
+	 */
 	public Division addDivision(Division division) {
 		getDivisions().add(division);
 		division.setSeason(this);
@@ -86,6 +147,10 @@ public class Season{
 		return division;
 	}
 
+	/** Removes a division to the list participating during the season.
+	 * @param division
+	 * @return
+	 */
 	public Division removeDivision(Division division) {
 		getDivisions().remove(division);
 		division.setSeason(null);
