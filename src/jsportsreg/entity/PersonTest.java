c6467f9e0279b16a2eae249a984f38dd83647af7
Travis Seiler <tseiler@xcellutions.com>
2013-04-14 19:55:19 -0400

    Last commit for the night......

--- src/jsportsreg/entity/PersonTest.java
+++ src/jsportsreg/entity/PersonTest.java
@@ -42,21 +42,44 @@ public class PersonTest {
 				"David", "404-890-0006", "T-Rex", "Parent/Guardian", "", "770-555-9999", new ArrayList<Address>(), 
 				new ArrayList<Person>(), new ArrayList<Player_Registration>() );
 		
-		assertEquals("Person Test 1: First Name", "Travis", instance1.getFirstName());
-		assertEquals("Person Test 1: Last Name", "Seiler", instance1.getLastName());
-		assertEquals("Person Test 1: Middle Name", "David", instance1.getMiddleName() );
-		assertEquals("Person Test 1: Nick Name", "T-Rex", instance1.getNickName() );
-		assertEquals("Person Test 1: Suffix Name", "", instance1.getSuffixName() );
-		assertEquals("Person Test 1: Home Phone Number", "404-759-0121", instance1.getHomePhone() );
-		assertEquals("Person Test 1: Mobile Phone Number", "404-890-0006", instance1.getMobilePhone() );
-		assertEquals("Person Test 1: Work Phone Number", "770-555-9999", instance1.getWorkPhone() );
-		assertEquals("Person Test 1: Birth Date", dateTest1, instance1.getBirthDate() );
-		assertEquals("Person Test 1: Gender", "Male", instance1.getGender() );
-		assertEquals("Person Test 1: Role", "Parent/Guardian", instance1.getRole() );
-		assertEquals("Person Test 1: Person ID", 21, instance1.getPersonID() );
-		assertEquals("Person Test 1: Player Registrations", true, instance1.getPlayerRegistrations().isEmpty() );
-		assertEquals("Person Test 1: Addresses", true, instance1.getAddresses().isEmpty() );
-		assertEquals("Person Test 1: Emergency Contacts", true, instance1.getEmergencyContacts().isEmpty() );
+		assertEquals("Person Test 2: First Name", "Travis", instance1.getFirstName());
+		assertEquals("Person Test 2: Last Name", "Seiler", instance1.getLastName());
+		assertEquals("Person Test 2: Middle Name", "David", instance1.getMiddleName() );
+		assertEquals("Person Test 2: Nick Name", "T-Rex", instance1.getNickName() );
+		assertEquals("Person Test 2: Suffix Name", "", instance1.getSuffixName() );
+		assertEquals("Person Test 2: Home Phone Number", "404-759-0121", instance1.getHomePhone() );
+		assertEquals("Person Test 2: Mobile Phone Number", "404-890-0006", instance1.getMobilePhone() );
+		assertEquals("Person Test 2: Work Phone Number", "770-555-9999", instance1.getWorkPhone() );
+		assertEquals("Person Test 2: Birth Date", dateTest1, instance1.getBirthDate() );
+		assertEquals("Person Test 2: Gender", "Male", instance1.getGender() );
+		assertEquals("Person Test 2: Role", "Parent/Guardian", instance1.getRole() );
+		assertEquals("Person Test 2: Person ID", 21, instance1.getPersonID() );
+		assertEquals("Person Test 2: Player Registrations", true, instance1.getPlayerRegistrations().isEmpty() );
+		assertEquals("Person Test 2: Addresses", true, instance1.getAddresses().isEmpty() );
+		assertEquals("Person Test 2: Emergency Contacts", true, instance1.getEmergencyContacts().isEmpty() );
+		
+		instance0.setFirstName("Alexzander");
+		instance0.setMiddleName("Edward");
+		instance0.setLastName("Seiler");
+		instance0.setSuffixName("");
+		instance0.setNickName("Zander");
+		Date dateTest2 = df.parse("10-3-2010");
+		
+		assertEquals("Person Test 3: First Name", "Travis", instance0.getFirstName());
+		assertEquals("Person Test 3: Last Name", "Seiler", instance0.getLastName());
+		assertEquals("Person Test 3: Middle Name", "David", instance0.getMiddleName() );
+		assertEquals("Person Test 3: Nick Name", "T-Rex", instance0.getNickName() );
+		assertEquals("Person Test 3: Suffix Name", "", instance0.getSuffixName() );
+		assertEquals("Person Test 3: Home Phone Number", "404-759-0121", instance0.getHomePhone() );
+		assertEquals("Person Test 3: Mobile Phone Number", "404-890-0006", instance0.getMobilePhone() );
+		assertEquals("Person Test 3: Work Phone Number", "770-555-9999", instance0.getWorkPhone() );
+		assertEquals("Person Test 3: Birth Date", dateTest1, instance0.getBirthDate() );
+		assertEquals("Person Test 3: Gender", "Male", instance0.getGender() );
+		assertEquals("Person Test 3: Role", "Parent/Guardian", instance0.getRole() );
+		assertEquals("Person Test 3: Person ID", 21, instance0.getPersonID() );
+		assertEquals("Person Test 3: Player Registrations", true, instance0.getPlayerRegistrations().isEmpty() );
+		assertEquals("Person Test 3: Addresses", true, instance0.getAddresses().isEmpty() );
+		assertEquals("Person Test 3: Emergency Contacts", true, instance0.getEmergencyContacts().isEmpty() );
 	}
 
 }
