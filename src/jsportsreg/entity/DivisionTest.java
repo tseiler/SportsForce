a5cbb8b86b5e7295a4e6d136e7bb270cb6aed1af
rkinsley <rkinsley@uga.edu>
2013-04-16 21:05:03 -0300

    Update DivisionTest.java

--- src/jsportsreg/entity/DivisionTest.java
+++ src/jsportsreg/entity/DivisionTest.java
@@ -4,11 +4,46 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Ryan M. Kinsley
+ * MIST 7570, Spring 2013
+ * Dr. Dan Everett
+ * @author rkinsley
+ *
+ */
+
 public class DivisionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDivision() {
+			
+		Division instance0 = new Division();
+			
+		assertEquals("Division Test 1: Division Description","",instance0.getDivisionDescription());
+		assertEquals("Division Test 1: Division Name","",instance0.getDivisionName());
+		// Don't test the season creation, separate tests for Season class.
+		// Don't test the sport creation, separate tests for Sport class.
+		// Don't test the playerRegistration creation, separate tests for Player Registration class.
+		
+		Season season0 = new Season();
+		Sport sport0 = new Sport();
+		Player_Registration playerregistration0 = new Player_Registration();
+		
+		Division instance1 = new Division(1, "T-Ball: 5 & 6 yoa","5 & 6 years old", season0, sport0, 
+				playerregistration0);
+		
+		assertEquals("Division Test 1: Division Description","T-Ball: 5 & 6 yoa",instance0.getDivisionDescription());
+		assertEquals("Division Test 1: Division Name","5 & 6 years old",instance0.getDivisionName());
+		
+		instance0.setDivisionID(1);
+		instance0.setDivisionDescription("Fast Pitch: 9 & 10 yoa");
+		instance0.setDivisionName("9 & 10 years old");
+		
+		assertEquals("Division Test 1: Division Description","Fast Pitch: 9 & 10 yoa",instance0.getDivisionDescription());
+		assertEquals("Division Test 1: Division Name","9 & 10 years old",instance0.getDivisionName());
+		
+
 	}
 
 }
+
